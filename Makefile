8fdce79301ea99d404e12bbf14f1e598a53d27eb
Michael Jenkins <managedkaos@users.noreply.github.com>
2021-03-26 16:54:19 -0700

    Add simulated analysis

--- Makefile
+++ Makefile
@@ -3,11 +3,11 @@ SHELL_FILES  = $(shell find . -maxdepth 1 -type f -name \*.sh)
 PYTHON_FILES = $(shell find . -maxdepth 1 -type f -name \*.py)
 APPLICATION  = $(shell find . -maxdepth 1 -type f -name \*.py | sort | grep -v config)
 
+all:
+	$(PYTHON) ./analyze-candidate.py
+
 lint:
 	flake8 --statistics $(PYTHON_FILES)
 
 requirements:
 	$(PYTHON) -m pip install -U -r requirements.txt
-
-all:
-	$(PYTHON) ./analyze-candidate.py
--- analyze-candidate.py
+++ analyze-candidate.py
@@ -1,10 +1,27 @@
+from datetime import datetime
 import random
+import time
 
+# Get the current date and time
+now = datetime.now()
+
+# Identity the blood groups
 blood_groups = ['A-', ' B+', 'B-', 'O+', 'O-', 'AB+', 'AB-']
+
+# Use a dictionary as an analysis function
 analysis = {'A-': 'MAYBE', ' B+': 'MAYBE', 'B-': 'MAYBE', 'O+': 'PERFECT', 'O-': 'GOOD', 'AB+': 'POSSIBLE', 'AB-': 'POSSIBLE'}
 
+# Generate a row of data using a random ID and blood group
 data = {'id': random.randint(1, 500), 'blood_group': random.choice(blood_groups)}
 
+# Start the analysis
+dt_string = now.strftime("%Y-%m-%d %H:%M")
+print("{} Beginning analysis".format(dt_string))
+
+# Simulate the analysis time by sleeping for up to 1 minute
+time.sleep(random.randint(1, 60))
+
+# Report the analysis
 print("CANDIDATE  : {}".format(data['id']))
 print("BLOOD GROUP: {}".format(data['blood_group']))
 print("ANALYSIS   : {}".format(analysis[data['blood_group']]))
