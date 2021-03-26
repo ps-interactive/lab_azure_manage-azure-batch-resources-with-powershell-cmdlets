36d557b7b2c51f314685edb2e0569e904c4e28c3
Michael Jenkins <managedkaos@users.noreply.github.com>
2021-03-26 16:37:01 -0700

    Full steam ahead

---
+++ Makefile
@@ -0,0 +1,13 @@
+PYTHON       = $(shell which python)
+SHELL_FILES  = $(shell find . -maxdepth 1 -type f -name \*.sh)
+PYTHON_FILES = $(shell find . -maxdepth 1 -type f -name \*.py)
+APPLICATION  = $(shell find . -maxdepth 1 -type f -name \*.py | sort | grep -v config)
+
+lint:
+	flake8 --statistics $(PYTHON_FILES)
+
+requirements:
+	$(PYTHON) -m pip install -U -r requirements.txt
+
+all:
+	$(PYTHON) ./analyze-candidate.py
--- analyze-candidate.py
+++ analyze-candidate.py
@@ -1,10 +1,9 @@
-import sys
 import random
 
 blood_groups = ['A-', ' B+', 'B-', 'O+', 'O-', 'AB+', 'AB-']
 analysis = {'A-': 'MAYBE', ' B+': 'MAYBE', 'B-': 'MAYBE', 'O+': 'PERFECT', 'O-': 'GOOD', 'AB+': 'POSSIBLE', 'AB-': 'POSSIBLE'}
 
-data = {'id': random.randint(1,500), 'blood_group': random.choice(blood_groups)}
+data = {'id': random.randint(1, 500), 'blood_group': random.choice(blood_groups)}
 
 print("CANDIDATE  : {}".format(data['id']))
 print("BLOOD GROUP: {}".format(data['blood_group']))
